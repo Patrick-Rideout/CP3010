d302fd4950acb423677e1731f2c9220ee5dc476b
Patrick Rideout <prideout2002@gmail.com>
2025-02-18 15:01:39 -0330

    Update to Pauls requirements.

--- myapp/controllers/register.controller.js
+++ myapp/controllers/register.controller.js
@@ -1,31 +1,37 @@
-const Course = require("../models/course.model.js");
+const Course = require("../models/course.model.js"); 
 const Student = require("../models/student.model.js");
 
 const registerStudentToCourse = async (req, res) => {
   try {
-    const { studentId, courseId } = req.body;
+    const { schoolId, courseName } = req.body;
 
-    const course = await Course.findById(courseId);
-    const student = await Student.findById(studentId);
+    console.log("schoolId: " + schoolId);
+    console.log("courseName: " + courseName);
+
+    const student = await Student.findOne({ schoolId: schoolId });
+
+    const course = await Course.findOne({ courseName });
+    
+    console.log(student)
+    console.log(course)
 
     if (!course) {
-      return res.status(500).json({ message: "ERROR: Course not found" });
+      return res.status(500).json({ message: "ERROR: Course not found" + course});
     }
 
     if (!student) {
-      return res.status(500).json({ message: "ERROR: Student not found" });
+      return res.status(500).json({ message: "ERROR: Student not found" + student});
     }
 
-    const studentsCourses = await Course.find({ students: studentId });
+    const studentsCourses = await Course.find({ students: student._id });
 
     for (let studentsCourse of studentsCourses) {
-
       for (let studentsSession of studentsCourse.sessions) {
-
         for (let newCourseSession of course.sessions) {
-
-          if (studentsSession.day === newCourseSession.day && (studentsSession.startTime < newCourseSession.startTime + newCourseSession.duration &&
-              studentsSession.startTime + studentsSession.duration > newCourseSession.startTime)
+          if (
+            studentsSession.day === newCourseSession.day &&
+            studentsSession.startTime < newCourseSession.startTime + newCourseSession.duration &&
+            studentsSession.startTime + studentsSession.duration > newCourseSession.startTime
           ) {
             return res.status(500).json({
               message: "ERROR: Scheduling conflict with another course",
@@ -35,8 +41,8 @@ const registerStudentToCourse = async (req, res) => {
       }
     }
 
-    course.students.push(studentId);
-    student.courses.push(courseId);
+    course.students.push(student._id);
+    student.courses.push(course._id);
 
     await course.save();
     await student.save();
